90082e02d1c4daa4bfacd430b61e2485a38af4a4
Greg <greg_jett92@tutanota.com>
2022-11-29 13:40:27 -0600

    First Test

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 
-const navbar = (
-    <nav>
-        <h1>Jimmy John's</h1>
+import React from "react"
+import ReactDOM from "react-dom"
+
+const page = (
+    <div>
+        <img src="./react-logo.png" width="40px" />
+        <h1>Fun facts about React</h1>
         <ul>
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
         </ul>
-    </nav>
+    </div>
 )
+
+ReactDOM.render(page, document.getElementById("root"))
+// const navbar = (
+//     <nav>
+//         <h1>Jimmy John's</h1>
+//         <ul>
+//             <li>Pricing</li>
+//             <li>About</li>
+//             <li>Contact</li>
+//         </ul>
+//     </nav>
+// )
 
 // function Navbar() {
 //     return (
