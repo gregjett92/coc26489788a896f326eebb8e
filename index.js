74bdf1fc319e926fe182439b479e690be09c9f95
Greg <greg.jett25@gmail.com>
2022-11-30 22:11:49 -0600

    Parent/Child Compent Exercise

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+/**
+Challenge: 
+
+- Move the `footer` into its own component called "Footer" 
+  and render that component inside the Page component.
+- Move the `h1` and `ol` together into another component
+  called "MainContent" and render inside Page as well.
+*/
 
 function Header() {
     return (
@@ -9,10 +20,17 @@ function Header() {
     )
 }
 
-function Page() {
+function Footer() {
+    return (
+        <footer>
+            <small>© 2021 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function MainContent() {
     return (
         <div>
-            <Header />
             <h1>Reasons I'm excited to learn React</h1>
             <ol>
                 <li>It's a popular library, so I'll be 
@@ -20,15 +38,21 @@ function Page() {
                 <li>I'm more likely to get a job as a developer
                 if I know React</li>
             </ol>
-            <footer>
-                <small>© 2021 Ziroll development. All rights reserved.</small>
-            </footer>
+        </div>
+    )
+}
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
         </div>
     )
 }
 
 ReactDOM.render(<Page />, document.getElementById("root"))
-
 
 
 
